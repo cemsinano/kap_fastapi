62e0def4d3578a282dbe3db1fdf9de9610ac1301
cemsinan <dezpyw-wyjqej-2Senwu>
2021-05-30 01:09:46 -0400

    .

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ def read_financial(tick: str):
     # comp = BISTCompany(ticker = tick)
     # pr = comp.get_financial_reports()
     pr = collection.find({'ticker': tick})
-    #print(pr[0])
 
-    return  json.loads(json_util.dumps(pr[0]))
+    json.loads(json.dumps(pr[0], default=json_util.default, ensure_ascii=False))
+
+    # return json.loads(json_util.dumps(pr[0]))
 
 
 
