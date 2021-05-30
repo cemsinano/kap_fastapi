bbd643404cd40d1961c7d1c96780042b11d7ecf5
cemsinan <dezpyw-wyjqej-2Senwu>
2021-05-29 21:25:26 -0400

    s

--- main.py
+++ main.py
@@ -1,64 +1,38 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-from typing import Optional
-
-from fastapi import FastAPI
-from pydantic import BaseModel, NameEmail
-#from pykap import BISTCompany, get_general_info
-
-from pymongo import MongoClient
-from bson import json_util
-import json
-
-client = MongoClient(
-    "mongodb+srv://cemsinan:cemsinanUserPassword@cluster0.ombz1.mongodb.net/mikroskop?retryWrites=true&w=majority")
-
-db = client['mikroskop']
-collection = db['bist_companies']
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
+@app.get('/')
+def hello_world():
+    '''
+    The root route which returns a JSON response.
 
-class Subscriber(BaseModel):
-    full_name: str
-    email: NameEmail
+    The JSON response is delivered as:
 
-
-@app.post("/s")
-async def newUser(sub: Subscriber):
-    name = sub.full_name
-    email = sub.email
-
-    # you could also return sub.json() or sub.dict()
-    return {
-        "full_name": name,
-        "email": email
+    {
+      'message': 'Hello, World!'
     }
+    '''
+    return {'message': 'Hello, World!'}
 
+@app.get('/healthcheck', status_code=status.HTTP_200_OK)
+def perform_healthcheck():
+    '''
+    Simple route for the GitHub Actions to healthcheck on.
 
-#@app.get("/subs/{sub_id}")
-#async def read_item(sub_id: int, q: Optional[str] = None):
-#    return {"sub_id": sub_id, "q": q}
+    More info is available at:
+    https://github.com/akhileshns/heroku-deploy#health-check
 
-#@app.get("/")
-#async def read_root():
-#    return {"message": "Welcome to this fantastic app!"}
+    It basically sends a GET request to the route & hopes to get a "200"
+    response code. Failing to return a 200 response code just enables
+    the GitHub Actions to rollback to the last version the project was
+    found in a "working condition". It acts as a last line of defense in
+    case something goes south.
 
+    Additionally, it also returns a JSON response in the form of:
 
-@app.get("/stock/{tick}")
-async def read_financial(tick: str):
-    # comp = BISTCompany(ticker = tick)
-    # pr = comp.get_financial_reports()
-    pr = collection.find({'ticker': tick})
-    print(pr[0])
-
-    return json.loads(json_util.dumps(pr[0]))
-
-
-#@app.get("/general/{tick}")
-#def read_general(tick: str):
-#    gi = get_general_info(tick=tick)
-#    return gi
+    {
+      'healtcheck': 'Everything OK!'
+    }
+    '''
+    return {'healthcheck': 'Everything OK!'}
