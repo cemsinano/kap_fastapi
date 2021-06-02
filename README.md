1594ef4bc32f3cdf7962dc59533214165ab84552
cemsinan <dezpyw-wyjqej-2Senwu>
2021-06-01 22:29:11 -0400

    .

---
+++ README.md
@@ -0,0 +1,10 @@
+## kap_fastapi
+
+Basic FastAPI and MongoDB implementation for KAP(Public Disclosure Platform of Turkey) Announcements.
+
+### Deployed on Heroku:
+
+It can be accessed via [this link](https://shielded-dawn-32422.herokuapp.com/docs).
+
+
+
--- main.py
+++ main.py
@@ -3,27 +3,21 @@ from fastapi import FastAPI, status
 from pymongo import MongoClient
 from bson import json_util
 import json
+from pykap import get_general_info
+import os
+from dotenv import load_dotenv, find_dotenv
 
-client = MongoClient(
-    "mongodb+srv://cemsinan:cemsinanUserPassword@cluster0.ombz1.mongodb.net/mikroskop?retryWrites=true&w=majority")
+load_dotenv(find_dotenv())
+
+MONGODB_URL = os.environ.get("MONGODB_URL")
+
+client = MongoClient(MONGODB_URL)
 
 db = client['mikroskop']
 collection = db['bist_companies']
 
 
-
 app = FastAPI()
-
-
-@app.get("/stock/{tick}")
-def read_financial(tick: str):
-    # comp = BISTCompany(ticker = tick)
-    # pr = comp.get_financial_reports()
-    pr = collection.find({'ticker': tick})
-
-    return json.loads(json_util.dumps(pr[0]))
-
-
 
 
 
@@ -35,28 +29,23 @@ def welcome():
     The JSON response is delivered as:
 
     '''
-    return {'message': 'Welcome!'}
+    return {'message': 'Welcome please go to /docs for more info!'}
 
-"""
-@app.get('/healthcheck', status_code=status.HTTP_200_OK)
-def perform_healthcheck():
-    '''
-    Simple route for the GitHub Actions to healthcheck on.
 
-    More info is available at:
-    https://github.com/akhileshns/heroku-deploy#health-check
 
-    It basically sends a GET request to the route & hopes to get a "200"
-    response code. Failing to return a 200 response code just enables
-    the GitHub Actions to rollback to the last version the project was
-    found in a "working condition". It acts as a last line of defense in
-    case something goes south.
+@app.get("/stock/{tick}")
+def read_financial(tick: str):
+    # comp = BISTCompany(ticker = tick)
+    # pr = comp.get_financial_reports()
+    pr = collection.find({'ticker': tick})
 
-    Additionally, it also returns a JSON response in the form of:
+    return json.loads(json_util.dumps(pr[0]))
 
-    {
-      'healtcheck': 'Everything OK!'
-    }
-    '''
-    return {'healthcheck': 'Everything OK!'}
-"""
+@app.get("/general/{tick}")
+def read_general(tick: str):
+    gi = get_general_info(tick=tick)
+    return gi
+
+
+
+
